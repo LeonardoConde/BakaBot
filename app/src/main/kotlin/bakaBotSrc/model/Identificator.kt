d7ea7b9467c107bbfde9d1f58549b6ff9675d045
KhaoYu <75866203+KhaoYu@users.noreply.github.com>
2021-11-07 22:36:22 -0300

    Send+Identificator

--- app/src/main/kotlin/bakaBotSrc/model/Identificator.kt
+++ app/src/main/kotlin/bakaBotSrc/model/Identificator.kt
@@ -1,4 +1,24 @@
 package bakaBotSrc.model
 
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.TextChannel
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
 class Identificator {
+    var channel: TextChannel? = null
+    var guild: Guild? = null
+
+    constructor(event: GuildMessageReceivedEvent) {
+        setChannel(event)
+        setGuild(event)
+    }
+
+    fun setChannel(event: GuildMessageReceivedEvent){
+        channel = event.channel
+    }
+
+    fun setGuild(event: GuildMessageReceivedEvent){
+        guild = event.guild
+    }
+
 }
--- app/src/main/kotlin/bakaBotSrc/model/MessageManager.kt
+++ app/src/main/kotlin/bakaBotSrc/model/MessageManager.kt
@@ -1,4 +1,22 @@
 package bakaBotSrc.model
 
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.TextChannel
+
 class MessageManager {
+    var channel: TextChannel? = null
+    var guild: Guild? = null
+    var identificator: Identificator? = null
+
+    constructor(identificator: Identificator) {
+        this.identificator = identificator
+        this.channel = this.identificator!!.channel
+        this.guild = this.identificator!!.guild
+    }
+
+    fun send(text: String): Boolean {
+        channel?.sendMessage(text)?.queue { }
+        return true
+    }
+
 }
