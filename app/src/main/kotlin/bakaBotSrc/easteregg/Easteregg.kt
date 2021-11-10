b58a75aafc2a0d7353053672695daa0798dc357a
KhaoYu <75866203+KhaoYu@users.noreply.github.com>
2021-11-10 00:03:22 -0300

    Eater Egg

---
+++ app/src/main/kotlin/bakaBotSrc/easteregg/Easteregg.kt
@@ -0,0 +1,15 @@
+package bakaBotSrc.easteregg
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
+class Easteregg {
+    var event: GuildMessageReceivedEvent? = null
+
+    constructor(event: GuildMessageReceivedEvent){
+        this.event = event
+    }
+
+    fun livia(){
+        event?.channel?.sendMessage("https://tenor.com/view/i-love-livia-i-love-you-livia-gif-20398101")?.queue()
+    }
+}
