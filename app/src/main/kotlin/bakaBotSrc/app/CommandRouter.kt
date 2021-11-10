5fbe97dad1a5ff5a1d898980ebf78050aa5f5bb5
KhaoYu <75866203+KhaoYu@users.noreply.github.com>
2021-11-10 00:06:56 -0300

    Revert "Merge branch 'dev' of https://github.com/LeonardoConde/BakaBot into dev"
    This reverts commit 6dfcf3c209b40fc07baed66d6484b85d3710a41d, reversing
    changes made to 8942c39405a0755450474ffad40d0942ee6b239c.

--- app/src/main/kotlin/bakaBotSrc/app/CommandRouter.kt
+++ app/src/main/kotlin/bakaBotSrc/app/CommandRouter.kt
@@ -1,7 +1,24 @@
 package bakaBotSrc.app
 
+import bakaBotSrc.model.AudioManager
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
 class CommandRouter {
-    fun router(id:Long,args:List<String>){
+
+    fun router(args:List<String>,event: GuildMessageReceivedEvent){
         println("Router says hi!")
+        when(args[0].toLowerCase()){
+            "help" -> println("Comando Help")
+            "playaudio" -> println("Comando PlayAudio")
+            "signup" -> println("Comando SignUP")
+            "songgame" -> println("Comando SongGame")
+            "join" -> AudioManager(event).join()
+            "leave" -> AudioManager(event).leave()
+
+            else -> {
+                println("Comando invalido!")
+            }
+        }
+
     }
 }
